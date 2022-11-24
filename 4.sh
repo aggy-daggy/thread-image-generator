fb5f51823bf107ba9907bae627e70668e61a69f2
aggy-daggy <118865325+aggy-daggy@users.noreply.github.com>
2022-11-24 03:41:38 +0100

    Update 4.sh

--- 4.sh
+++ 4.sh
@@ -6,7 +6,7 @@ COLOR=$(openssl rand -hex 3)
 #convert overlay.png -filter point -scale 200% overlay_2x.png
 #convert overlay.png -filter point -scale 300% -fuzz 100% -fill "#$COLOR" -opaque black overlay_2x.png
 convert hq.png -filter point -scale 150% -fuzz 100% -fill "#$COLOR" -opaque black overlay_2x.png
-convert overlay_2x.png -gravity center -pointsize 55 -undercolor "#$COLOR" -fill transparent -font "ttf/TerminusTTF-4.49.2.ttf" -annotate +0+200 'like just make game' -append overlay_2x.png
+convert overlay_2x.png -gravity center -pointsize 55 -undercolor "#$COLOR" -fill transparent -font "ttf/TerminusTTF-4.49.2.ttf" -annotate +0+200 'just like make game' -append overlay_2x.png
 
 
 montage $(ls -d resized/* | sort -R | tail -30) -geometry +1+1 result.jpg
